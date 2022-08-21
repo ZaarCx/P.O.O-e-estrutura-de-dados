1fc52b78e0077cb68d3914b586478074174ec353
ZaarCx <111471135+ZaarCx@users.noreply.github.com>
2022-08-21 14:22:53 -0300

    first commit

---
+++ Conta.java
@@ -0,0 +1,49 @@
+package primeiro.projeto;
+import java.util.Scanner;
+public class Conta
+{
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("digite um número: ");
+        int A = input.nextInt();
+        System.out.print("digite outro número: ");
+        int B = input.nextInt();
+        int soma = A + B;
+        System.out.println("X = " + soma);
+        input.close();
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
